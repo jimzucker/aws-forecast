4fdc8a54f5afd511498579c2f3f9df8589cc9f5a
jimzucker <jim@zucker.us>
2020-12-19 20:36:19 -0800

    #1 working cloud formation doc

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
-aws s3 --profile jim-zucker cp get_forecast.py s3://jimzucker-github-getforecast
+rm -fr target
+mkdir target
+cd target
+zip get_forecast.zip ../get_forecast.py
+aws s3 --profile jim-zucker cp get_forecast.zip s3://jimzucker-github-getforecast
