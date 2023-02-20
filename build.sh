569c6aacd5be499826d1813c9d215ea4b6c5be32
Subash Chandra <subashc2023@gmail.com>
2023-02-20 13:27:59 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,5 +5,7 @@ zip get_forecast.zip ../get_forecast.py
 aws s3 --profile jim-zucker cp get_forecast.zip s3://jimzucker-github-getforecast
 cd ..
 
-# Check IAM Configuration for Instructions on how to upload updated code to this bucket.
+# Check IAM_Configuration for Instructions on how to upload updated code to this bucket.
+ # Link to IAM Configuration document: [IAM_Configuration](./IAM_Configuration.md)
+
 
